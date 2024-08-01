d0d8610ee630e67124e8b9ba1998e4991b7f9bf5
VmMartins <166064541+VmMartins@users.noreply.github.com>
2024-08-01 17:09:50 -0300

    Add files via upload

---
+++ vih_PHP/variaveis.php
@@ -0,0 +1,21 @@
+<?php
+
+//Variavél = reserva de um pedaço de memória.
+
+$nome = "Maria";
+$numero = 10;
+$num = 1.5;
+$num++;
+
+$res = $numero % 3;
+
+var_dump($nome);
+echo"<br>";
+var_dump($numero);
+echo"<br>";
+var_dump($num);
+echo"<br>";
+var_dump($res);
+
+
+?>
